d353336be8ec28f947c54628d356955b8e4a2688
tjmtmmnk <tsujimoto027@gmail.com>
2020-01-07 01:11:45 +0900

    create voter managed by admin entity

--- administrator.py
+++ administrator.py
@@ -6,25 +6,26 @@ from umbral.kfrags import KFrag
 from umbral.pre import Capsule, encrypt, generate_kfrags
 from umbral.signing import Signer
 
+from adminvoter import AdminVoter
 from bulletinboard import BulletinBoard
-
-NUM_OF_VOTER = 5
-NUM_OF_CANDIDATE = 3
+from config import NUM_OF_VOTER
+from proxy import Proxy
 
 
 class Administrator:
     def __init__(self):
         set_default_curve()
         self.bb = BulletinBoard.get_instance()
+        self.proxy = Proxy.get_instance()
         self.private_key = UmbralPrivateKey.gen_key()
         self.public_key = self.private_key.get_pubkey()
         self.singning_key = UmbralPrivateKey.gen_key()
         self.verifying_key = self.singning_key.get_pubkey()
         self.signer = Signer(private_key=self.private_key)
-        self.voter_keys = []
+        self.voters = []
 
     def setup(self, cand_public_keys: UmbralPublicKey):
-        self._generate_key_pair_for_voter()
+        self._generate_voters()
         re_enc_keys = self._generate_re_enc_key_admin_to_voter()
 
         self.bb.append({"admin_public_key": self.public_key})
@@ -33,17 +34,25 @@ class Administrator:
         self.bb.append({"hash_algorithm": "SHA256"})
 
     def credential_dispatch(self):
-        for voter_key in self.voter_keys:
-            credential = self._generate_credential(voter_key["public"])
-            ciphertext, capsule = self._encrypt_credential_and_voter_short_private_key(
+        for voter in self.voters:
+            credential = self._generate_credential(voter.short_public_key)
+            (ciphertext, capsule) = self._encrypt_credential_and_voter_short_private_key(
                 credential,
-                voter_key["private"]
+                voter.short_private_key
             )
-            # TODO: send to proxy
+            self.proxy.receive_ciphertext_from_admin((ciphertext, capsule))
 
     def _generate_credential(self,
                              voter_short_public_key: UmbralPublicKey
                              ) -> Tuple[UmbralPublicKey, Signer]:
+        """generate credential
+
+        Arguments:
+            voter_short_public_key {UmbralPublicKey}
+
+        Returns:
+            Tuple[UmbralPublicKey, Signer]
+        """
         signature = self.signer(voter_short_public_key.to_bytes())
         return (voter_short_public_key, signature)
 
@@ -51,33 +60,41 @@ class Administrator:
                                                         credential: Tuple[UmbralPublicKey, Signer],
                                                         private_key: UmbralPrivateKey
                                                         ) -> Tuple[bytes, Capsule]:
+        """encrypt credential and voter short private key
+
+        Arguments:
+            credential {Tuple[UmbralPublicKey, Signer]}
+            private_key {UmbralPrivateKey}
+
+        Returns:
+            Tuple[bytes, Capsule] -- need to save Capsule
+        """
         credential_bytes = credential[0].to_bytes() + bytes(credential[1])
         private_key_bytes = private_key.to_bytes()
         return encrypt(self.public_key, credential_bytes + private_key_bytes)
 
-    def get_public_key(self):
-        return self.public_key
-
-    def _generate_key_pair_for_voter(self):
+    def _generate_voters(self):
         for i in range(NUM_OF_VOTER):
-            voter_short_private_key = UmbralPrivateKey.gen_key()
-            voter_short_public_key = voter_short_private_key.get_pubkey()
-            self.voter_keys.append(
-                {
-                    "public": voter_short_public_key,
-                    "private": voter_short_private_key
-                }
-            )
+            short_private_key = UmbralPrivateKey.gen_key()
+            short_public_key = short_private_key.get_pubkey()
+            admin_voter = AdminVoter(short_public_key, short_private_key)
+            self.voters.append(admin_voter)
 
-    def _generate_re_enc_key_admin_to_voter(self) -> List[KFrag]:
+    def _generate_re_enc_key_admin_to_voter(self) -> List[Dict[str, KFrag]]:
+        """generate re-encrypt key from admin to (admin)voter
+
+        Returns:
+            List[Dict[str, KFrag]] -- voter id to re-encrypt
+        """
         re_enc_keys = []
-        for key in self.voter_keys:
+        for voter in self.voters:
             re_enc_key = generate_kfrags(
                 delegating_privkey=self.private_key,
                 signer=self.signer,
-                receiving_pubkey=key["public"],
+                receiving_pubkey=voter.short_public_key,
                 threshold=1,
                 N=1)
-
-            re_enc_keys.append(re_enc_key)
+            re_enc_keys.append({
+                voter.id: re_enc_key
+            })
         return re_enc_keys
--- proxy.py
+++ proxy.py
@@ -1,1 +1,33 @@
+from typing import Tuple
+
+from umbral.pre import Capsule
+
+from config import NUM_OF_VOTER
+
+
 class Proxy:
+    _unique_instance = None
+
+    def __new__(cls):
+        raise NotImplementedError('Cannot initialize via Constructor')
+
+    @classmethod
+    def __internal_new__(cls):
+        return super().__new__(cls)
+
+    @classmethod
+    def get_instance(cls):
+        if not cls._unique_instance:
+            cls.ciphertexts = []
+            cls._unique_instance = cls.__internal_new__()
+
+        return cls._unique_instance
+
+    def receive_ciphertext_from_admin(self, ciphertext: Tuple[bytes, Capsule]):
+        self.ciphertexts.append(ciphertext)
+
+    def transfer_credential_to_voter(self):
+        if len(self.ciphertexts) != NUM_OF_VOTER:
+            raise Exception(
+                "Not matched between num of voters and num of ciphertexts")
+        transferred_voter_ids = []
